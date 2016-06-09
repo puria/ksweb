895221c24c6a02c86ea213f68af61b541de23e91
Federico Fiori <federico.fiori@axant.it>
2016-06-09 14:31:41 +0200

    2.3 - Create simple a Advanced precondition

--- ksweb/ksweb/model/precondition.py
+++ ksweb/ksweb/model/precondition.py
@@ -8,19 +8,45 @@ from ksweb.model import DBSession
 
 
 class Precondition(MappedClass):
+    PRECONDITION_TYPE = [u"simple", u"advanced"]
+
     class __mongometa__:
         session = DBSession
         name = 'preconditions'
         indexes = [
-            ('_user',),
+            ('_owner',),
         ]
 
     _id = FieldProperty(s.ObjectId)
 
-    name = FieldProperty(s.String, required=False)
+    _owner = ForeignIdProperty('User')
+    owner = RelationProperty('User')
 
-    _user = ForeignIdProperty('User')
-    user = RelationProperty('User')
+    #_category = ForeignIdProperty('Category')
+    #category = RelationProperty('Category')
+
+    title = FieldProperty(s.String, required=False)
+    type = FieldProperty(s.OneOf(*PRECONDITION_TYPE), required=True)
+    condition = FieldProperty(s.Anything)
+    """
+    In case of type: simple
+    the condition is like: [ObjectId('qa'), 'String_response]
+
+    In case of type advanced
+    the condition is like: [ObjectId(precond_1), &, ObjectId(precond_2), | , ObjectId(precond_3)]
+    """
+
+    public = FieldProperty(s.Bool, if_missing=True)
+    visible = FieldProperty(s.Bool, if_missing=True)
+
+    @property
+    def evaluate(self):
+        if self.type == 'simple':
+            print "evaluate simple precondition"
+            return
+        if self.type == 'advanced':
+            print "evaluate advanced precondition"
+            return
 
 
 __all__ = ['Precondition']
