ec89f34b133cd0ee9402db6e70fa709ddcfb7deb
Federico Fiori <1395191+federicofiori@users.noreply.github.com>
2016-06-09 10:38:05 +0200

    2.3 - Form for simple precondition

--- ksweb/ksweb/tests/functional/test_validator.py
+++ ksweb/ksweb/tests/functional/test_validator.py
@@ -15,10 +15,19 @@ class TestValidators(TestController):
         else:
             assert True
 
-    def test_category_exist_validator_with_error(self):
+    def test_category_exist_validator_with_obj_not_valid(self):
         validator = CategoryExistValidator()
         try:
             res = validator._validate_python('not_obj_id')
+        except ValidationError:
+            assert True
+        else:
+            assert False
+
+    def test_category_exist_validator_with_not_existing_category(self):
+        validator = CategoryExistValidator()
+        try:
+            res = validator._validate_python('5757ce79c42d752bde919318')
         except ValidationError:
             assert True
         else:
