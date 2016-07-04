0c6eeac5cfda1e2233b40df68ac63cbfee2637e9
Vittorio Polto <8233312+vittoriopolto@users.noreply.github.com>
2016-07-04 15:49:43 +0200

    New fabfile and new stagin .ini

--- ksweb/fabfile.py
+++ ksweb/fabfile.py
@@ -13,6 +13,12 @@ APP_DEPLOY_BRANCH = 'default'
 APP_SUPPORTS_MIGRATIONS = False
 APP_USES_GEVENT = False # Keep in mind that enabling GEVENT requires requires WSGIApplicationGroup %{GLOBAL} in Apache VHost conf
 
+APP_NAME_AXANT = 'ksweb-axant/ksweb-axant'    # This is the directory where the app is cloned
+APP_ID_AXANT = '572'  # This is the USERID on the axantweb server
+APP_DEPLOY_BRANCH_AXANT = 'sprint2'
+APP_USES_GEVENT_AXANT = False # Keep in mind that enabling GEVENT requires requires WSGIApplicationGroup %{GLOBAL} in Apache VHost conf
+
+
 import time
 import fabric
 from fabric.api import *
@@ -22,13 +28,21 @@ from fabric.contrib.files import exists, append
 import glob, os
 
 APP_USER = 'aw%s' % APP_ID
+APP_USER_AXANT = 'aw%s' % APP_ID_AXANT
 
 env.hosts = ['%s@wsgi.axantweb.com' % APP_USER]
+env.hosts_axant = ['%s@wsgi.axantweb.com' % APP_USER_AXANT]
+
 
 APP_PATH = os.path.join('/var/www', APP_ID, 'app', APP_NAME)
 ENV_PATH = os.path.join('/var/www', APP_ID, 'penv')
 
+APP_PATH_AXANT = os.path.join('/var/www', APP_ID_AXANT, 'app', APP_NAME_AXANT)
+ENV_PATH_AXANT = os.path.join('/var/www', APP_ID_AXANT, 'penv')
+
+
 ENV_ACTIVATE = os.path.join(ENV_PATH, 'bin/activate')
+ENV_ACTIVATE_AXANT = os.path.join(ENV_PATH_AXANT, 'bin/activate')
 
 WSGI_APPLICATION = '''
 from paste.deploy import loadapp
@@ -39,12 +53,30 @@ def init_application(path):
     return application
 ''' % APP_NAME
 
+WSGI_APPLICATION_AXANT = '''
+    from paste.deploy import loadapp
+    
+    def init_application(path):
+    APP_CONFIG = path + "/%s/staging.ini"
+    application = loadapp("config:%%s" %% APP_CONFIG)
+    return application
+    ''' % APP_NAME_AXANT
+
+
+
 if APP_USES_GEVENT:
     WSGI_APPLICATION = '''import gevent
 from gevent import monkey
 monkey.patch_all()
-
 ''' + WSGI_APPLICATION
+
+if APP_USES_GEVENT_AXANT:
+    WSGI_APPLICATION_AXANT = '''import gevent
+        from gevent import monkey
+        monkey.patch_all()
+        
+        ''' + WSGI_APPLICATION_AXANT
+
 
 def _wait(seconds):
     for i in range(seconds, 0, -1):
@@ -59,6 +91,9 @@ def _wait(seconds):
 def _cmd_in_venv(cmd):
     return 'source %s; %s' % (ENV_ACTIVATE, cmd)
 
+def _cmd_in_venv_AXANT(cmd):
+    return 'source %s; %s' % (ENV_ACTIVATE_AXANT, cmd)
+
 
 def _first_deploy():
     # This is required to make gearbox available, otherwise only TurboGears gets installed
@@ -69,6 +104,17 @@ def _first_deploy():
         run('rm wsgi_application.py')
         append('wsgi_application.py', WSGI_APPLICATION)
         
+
+def _first_deploy_AXANT():
+    # This is required to make gearbox available, otherwise only TurboGears gets installed
+    run(_cmd_in_venv_AXANT('pip install "tg.devtools==2.3.8"'))
+    
+    with cd('app'):
+        run('hg clone %s' % APP_REPOSITORY)
+        run('rm wsgi_application.py')
+        append('wsgi_application.py', WSGI_APPLICATION_AXANT)
+
+
 
 def deploy_dev():
     is_first_deploy = not exists(APP_PATH)
@@ -94,6 +140,33 @@ def deploy_dev():
         run('touch /tmp/please_restart_apache_in_5_minutes')
         print colors.yellow('USING GEVENT THE SERVER WILL TAKE UP TO 5 MINUTES TO RESTART')
         _wait(60*5)
+
+
+def deploy_axant():
+    is_first_deploy = not exists(APP_PATH_AXANT)
+    if is_first_deploy:
+        print 'Performing First deploy!'
+        _first_deploy_AXANT()
+
+    with cd(APP_PATH_AXANT):
+        run('hg pull -u')
+        run('hg update -C %s' % APP_DEPLOY_BRANCH_AXANT)
+        run(_cmd_in_venv_AXANT('python setup.py develop'))
+        
+        if is_first_deploy:
+            run(_cmd_in_venv_AXANT('gearbox setup-app -c staging-axant.ini'))
+        else:
+            if APP_SUPPORTS_MIGRATIONS:
+                run(_cmd_in_venv_AXANT('gearbox migrate -c staging-axant.ini upgrade'))
+
+    if not APP_USES_GEVENT_AXANT:
+        run('touch run.wsgi')
+    else:
+        # GEVENT doesn't work with a plain touch, needs apache restart
+        run('touch /tmp/please_restart_apache_in_5_minutes')
+        print colors.yellow('USING GEVENT THE SERVER WILL TAKE UP TO 5 MINUTES TO RESTART')
+        _wait(60*5)
+
 
 
 def check_dev_log():
