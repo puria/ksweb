32d6fbcc346e2d4cb71883b047061f3c13a9111e
Lorenzo Giacosa <giacosalorenzo89@gmail.com>
2016-11-11 12:02:21 +0100

    4.4 - Added Resolve controller, now edit for output works

---
+++ ksweb/ksweb/controllers/precondition/simple.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""Precondition/simple controller module"""
+from bson import ObjectId
+from tg import expose, validate, RestController, decode_params, request, \
+    validation_errors_response,  response, tmpl_context
+from tw2.core import OneOfValidator, StringLengthValidator
+from ksweb import model
+from ksweb.lib.validator import QAExistValidator, CategoryExistValidator
+
+
+class PreconditionSimpleController(RestController):
+    def _before(self, *args, **kw):
+        tmpl_context.sidebar_section = "preconditions"
+
+
+    @expose('ksweb.templates.precondition.simple.new')
+    def new(self, **kw):
+        return dict(page='precondition-new')
+
+    @decode_params('json')
+    @expose('json')
+    @validate({
+        'title': StringLengthValidator(min=2),
+        'category': CategoryExistValidator(required=True),
+        'question': QAExistValidator(required=True),
+        'answer_type': OneOfValidator(values=[u'have_response', u'what_response'], required=True),
+    }, error_handler=validation_errors_response)
+    def post(self, title, category, question, answer_type, interested_response,  **kw):
+        user = request.identity['user']
+
+        #  CASO BASE in cui risco a creare una precondizione semplice per definizione e' quella di che venga solamente selezionata una risposta
+        if len(interested_response) == 1:
+            #  La risposta e' solo una creo una precondizione semplice
+            model.Precondition(
+                _owner=user._id,
+                _category=ObjectId(category),
+                title=title,
+                type='simple',
+                condition=[ObjectId(question), interested_response[0]]
+            )
+        else:
+            #  CASO AVANZATO sono state selezionate piu' risposte, devo prima creare tutte le precondizioni semplici e poi creare quella complessa
+            if answer_type == "have_response":
+                #  Create one precondition simple for all possibility answer to question
+                #  After that create a complex precondition with previous simple precondition
+                interested_response = model.Qa.query.get(_id=ObjectId(question)).answers
+
+            if answer_type == "what_response":
+                #  Create one precondition simple for all selected answer to question
+                #  After that create a complex precondition with previous simple precondition
+
+                if len(interested_response) <= 1:
+                    response.status_code = 412
+                    return dict(errors={'interested_response': 'Inserire almeno una risposta'})
+
+            base_precond = []
+            for resp in interested_response:
+                prec = model.Precondition(
+                    _owner=user._id,
+                    _category=ObjectId(category),
+                    title="_base_for_%s" % title,
+                    type='simple',
+                    condition=[ObjectId(question), resp],
+
+                    public=True,
+                    visible=False
+                )
+                base_precond.append(prec)
+
+            condition = []
+            for prc in base_precond[:-1]:
+                condition.append(prc._id)
+                condition.append('or')
+
+            condition.append(base_precond[-1]._id)
+
+            model.Precondition(
+                _owner=user._id,
+                _category=ObjectId(category),
+                title=title,
+                type='advanced',
+                condition=condition
+            )
+
+        return dict(errors=None)
---
+++ ksweb/ksweb/model/document.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""Document model module."""
+import tg
+from markupsafe import Markup
+from ming import schema as s
+from ming.odm import FieldProperty, ForeignIdProperty, RelationProperty
+from ming.odm.declarative import MappedClass
+from ksweb.model import DBSession
+
+
+def _custom_title(obj):
+    return Markup("<a href='%s'>%s</a>" % (tg.url('/document/edit', params=dict(_id=obj._id)), obj.title))
+
+
+def _content_preview(obj):
+    return Markup("Little preview of: %s" % obj._id)
+
+
+class Document(MappedClass):
+    class __mongometa__:
+        session = DBSession
+        name = 'documents'
+        indexes = [
+            ('_owner',),
+            ('public',),
+            ('title',),
+            ('_category',),
+        ]
+
+    __ROW_COLUM_CONVERTERS__ = {
+        'title': _custom_title,
+        'content': _content_preview
+    }
+
+    _id = FieldProperty(s.ObjectId)
+
+    _owner = ForeignIdProperty('User')
+    owner = RelationProperty('User')
+
+    _category = ForeignIdProperty('Category')
+    category = RelationProperty('Category')
+
+    title = FieldProperty(s.String, required=True)
+
+    content = FieldProperty(s.Anything, required=True)
+    """
+    Possible content of the document is a list with two elements type:
+        - text
+        - output
+
+        If the type is text the content contain the text
+        If the type is output the content contain the obj id of the related output
+
+
+        An example of the content is this
+        "content" : [
+            {
+                "content" : "575eb879c42d7518bb972256",
+                "type" : "output",
+                "title" : "ciao"
+            },
+            {
+                "content" : "Simple text",
+                "type" : "text",
+                "title" : ""
+            }
+        ]
+    """
+
+    public = FieldProperty(s.Bool, if_missing=True)
+    visible = FieldProperty(s.Bool, if_missing=True)
+
+    @classmethod
+    def document_available_for_user(cls, user_id):
+        return cls.query.find({'_owner': user_id}).sort('title')
+
+
+__all__ = ['Document']
