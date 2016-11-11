19cf3e25c3b3f64b6032a490b40628e96ba8bb15
Lorenzo Giacosa <giacosalorenzo89@gmail.com>
2016-11-11 12:02:21 +0100

    4.4 - Added Resolve controller, now edit for output works

---
+++ ksweb/ksweb/controllers/output.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""Output controller module"""
+import json
+
+from bson import ObjectId
+from ksweb.lib.predicates import CanManageEntityOwner
+from tg import expose, validate, validation_errors_response, RestController, decode_params, request, tmpl_context, \
+    response
+import tg
+from tg import redirect
+from tg.decorators import paginate, require
+from tg.i18n import lazy_ugettext as l_
+from tg import predicates
+from tw2.core import StringLengthValidator
+from ksweb import model
+from ksweb.lib.validator import CategoryExistValidator, PreconditionExistValidator, \
+    OutputExistValidator, OutputContentValidator
+from ksweb.controllers.resolve import ResolveController
+
+
+class OutputController(RestController):
+    def _validate_precondition_with_qa(self, precondition, content):
+        #  Check content precondition element
+        precond = model.Precondition.query.find({'_id': ObjectId(precondition)}).first()
+        related_qa = precond.response_interested
+        #  Check elem['content'] contain the obj id of the related
+        for elem in content:
+            if elem['type'] == 'qa_response':
+                if elem['content'] not in related_qa.keys():
+                    response.status_code = 412
+                    return dict(errors={'content': 'Domanda %s non legata alla precondizione utilizzata' % elem['title']})
+        return dict()
+
+    def _before(self, *args, **kw):
+        tmpl_context.sidebar_section = "outputs"
+        tmpl_context.id_obj=kw.get('_id')
+
+    allow_only = predicates.has_any_permission('manage', 'lawyer',  msg=l_('Only for admin or lawyer'))
+
+    @expose('ksweb.templates.output.index')
+    @paginate('entities', items_per_page=int(tg.config.get('pagination.items_per_page')))
+    def get_all(self, **kw):
+        return dict(
+            page='output-index',
+            fields={
+                'columns_name': ['Nome', 'Categoria', 'Precondizione', 'Testo'],
+                'fields_name': ['title', 'category', 'precondition', 'content']
+            },
+            entities=model.Output.output_available_for_user(request.identity['user']._id),
+            actions=True
+        )
+
+    @expose('json')
+    @expose('ksweb.templates.output.new')
+    def new(self, **kw):
+        tmpl_context.sidebar_output = "output-new"
+        return dict(output={}, errors=None)
+
+    @decode_params('json')
+    @expose('json')
+    @validate({
+        'title': StringLengthValidator(min=2),
+        'content': OutputContentValidator(),
+        'category': CategoryExistValidator(required=True),
+        'precondition': PreconditionExistValidator(required=True),
+    }, error_handler=validation_errors_response)
+    def post(self, title, content, category, precondition, **kw):
+        #  Check content precondition element
+        error = self._validate_precondition_with_qa(precondition, content)
+        if error:
+            return error
+
+        user = request.identity['user']
+        model.Output(
+            _owner=user._id,
+            _category=ObjectId(category),
+            _precondition=ObjectId(precondition),
+            title=title,
+            content=content,
+            public=True,
+            visible=True
+        )
+        return dict(errors=None)
+
+    @decode_params('json')
+    @expose('json')
+    @validate({
+        '_id': OutputExistValidator(required=True),
+        'title': StringLengthValidator(min=2),
+        'content': OutputContentValidator(),
+        'category': CategoryExistValidator(required=True),
+        'precondition': PreconditionExistValidator(required=True),
+    }, error_handler=validation_errors_response)
+    @require(CanManageEntityOwner(msg=u'Non puoi modificare questo output.', field='_id', entity_model=model.Output))
+    def put(self, _id, title, content, category, precondition, **kw):
+        #  Check content precondition element
+        error = self._validate_precondition_with_qa(precondition, content)
+        if error:
+            return error
+
+        res = ResolveController().get_related_entities(_id)
+
+        if res.get("entities"):
+            params = dict(
+                _id=_id,
+                title=title,
+                content=json.dumps(content, ensure_ascii=False),
+                category=category,
+                precondition=precondition,
+                entity='output',
+                **kw
+            )
+            return dict(redirect_url=tg.url('/resolve', params=params))
+
+        output = model.Output.query.find({'_id': ObjectId(_id)}).first()
+        output.title = title
+        output._category = ObjectId(category)
+        output._precondition = ObjectId(precondition)
+        output.content = content
+
+        return dict(errors=None, redirect_url=None)
+
+    @expose('ksweb.templates.output.new')
+    @validate({
+        '_id': OutputExistValidator(required=True)
+    }, error_handler=validation_errors_response)
+    @require(CanManageEntityOwner(msg=u'Non puoi modificare questo output.', field='_id', entity_model=model.Output))
+    def edit(self, _id, **kw):
+        output = model.Output.query.find({'_id': ObjectId(_id)}).first()
+
+        # cercare su quali documenti o outout è utilizzato questo output
+
+        output_related = model.Output.query.find({"content.type": "output", "content.content": _id}).all()
+        documents_realted = model.Document.query.find({"content.type": "output", "content.content": _id})
+
+        print "output related", [o.title for o in output_related]
+        print "document related", [d.title for d in documents_realted]
+
+        tmpl_context.sidebar_output = "output-edit"
+        return dict(output=output, errors=None)
+
+
+    @expose('json')
+    def sidebar_output(self, _id=None):
+        res = model.Output.query.aggregate([
+            {
+                '$match': {
+                    '_owner': request.identity['user']._id,
+                    '_id': {'$ne': ObjectId(_id)},
+                    'visible': True
+                }
+            },
+            {
+                '$group': {
+                    '_id': '$_category',
+                    'output': {'$push': "$$ROOT",}
+                }
+            }
+        ])['result']
+
+        #  Insert category name into res
+        for e in res:
+            e['category_name'] = model.Category.query.get(_id=ObjectId(e['_id'])).name
+
+        return dict(outputs=res)
+
+    @expose('json')
+    @decode_params('json')
+    @validate({
+        'id': OutputExistValidator(required=True),
+    }, error_handler=validation_errors_response)
+    def human_readable_details(self, id,  **kw):
+        output = model.Output.query.get(_id=ObjectId(id))
+
+        return dict(output={
+            '_id': output._id,
+            'title': output.title,
+            'content': output.human_readbale_content,
+            'human_readbale_content': output.human_readbale_content,
+            '_owner': output._owner,
+            'owner': output.owner.display_name,
+            '_precondition': output._precondition,
+            'precondition': output.precondition.title,
+            '_category': output._category,
+            'category': output.category.name,
+            'public': output.public,
+            'visible': output.visible,
+            'created_at': output.created_at
+        })
---
+++ ksweb/ksweb/controllers/qa.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""Qa controller module"""
+from bson import ObjectId
+from tg import expose, validate, validation_errors_response, response, RestController, \
+    decode_params, request, tmpl_context
+import tg
+from tg.decorators import paginate
+from tg.i18n import lazy_ugettext as l_
+from tg import predicates
+from tw2.core import StringLengthValidator, OneOfValidator
+from ksweb import model
+from ksweb.lib.validator import CategoryExistValidator, QAExistValidator, PreconditionExistValidator
+
+
+class QaController(RestController):
+    def _before(self, *args, **kw):
+        tmpl_context.sidebar_section = "qas"
+
+    allow_only = predicates.has_any_permission('manage', 'lawyer',  msg=l_('Only for admin or lawyer'))
+
+    @expose('ksweb.templates.qa.index')
+    @paginate('entities', items_per_page=int(tg.config.get('pagination.items_per_page')))
+    def get_all(self, **kw):
+        return dict(
+            page='qa-index',
+            fields={
+                'columns_name': ['Name', 'Category', 'Precondition', 'Question', 'Type'],
+                'fields_name': ['title', 'category', 'parent_precondition', 'question', 'type']
+            },
+            entities=model.Qa.qa_available_for_user(request.identity['user']._id),
+            actions=True
+        )
+
+    @expose('json')
+    @validate({
+        'id': QAExistValidator(required=True),
+    }, error_handler=validation_errors_response)
+    def get_one(self, id,  **kw):
+        qa = model.Qa.query.find({'_id': ObjectId(id), '_owner': request.identity['user']._id}).first()
+        return dict(qa=qa)
+
+    @expose('json')
+    def get_single_or_multi_question(self):
+        questions = model.Qa.query.find({
+            'type': {'$in': ['single', 'multi']},
+            '_owner': request.identity['user']._id
+        }).all()
+        return dict(questions=[{'_id': qa._id, 'title': qa.title} for qa in questions])
+
+    @expose('json')
+    @expose('ksweb.templates.qa.new')
+    def new(self, **kw):
+        return dict(errors=None)
+
+    @decode_params('json')
+    @expose('json')
+    @validate({
+        'title': StringLengthValidator(min=2),
+        'category': CategoryExistValidator(required=True),
+        'question': StringLengthValidator(min=2),
+        'tooltip': StringLengthValidator(min=0, max=100),
+        'link': StringLengthValidator(min=0, max=100),
+        'answer_type': OneOfValidator(values=model.Qa.QA_TYPE, required=True),
+        'precondition': PreconditionExistValidator(required=False),
+    }, error_handler=validation_errors_response)
+    def post(self, title, category, question, tooltip, link, answer_type, precondition=None, answers=None, **kw):
+
+        if answer_type == "single" or answer_type == "multi":
+            if len(answers) < 2:
+                response.status_code = 412
+                return dict(
+                    errors={'answers': 'Inserire almeno due risposte'})
+
+        user = request.identity['user']
+
+        qa = model.Qa(
+                _owner=user._id,
+                _category=ObjectId(category),
+                _parent_precondition=ObjectId(precondition) if precondition else None,
+                title=title,
+                question=question,
+                tooltip=tooltip,
+                link=link,
+                type=answer_type,
+                answers=answers,
+                public=True,
+                visible=True
+            )
+
+        if answer_type == 'text':  # model.Qa.QA_TYPE[0]
+                        model.Precondition(
+                            _owner=user._id,
+                            _category=ObjectId(category),
+                            title=title + ' compilata',
+                            type='simple',
+                            condition=[qa._id, ''])
+
+        return dict(errors=None)
