e97e41ba3f6e97cfc1ed54eb5b3dff5c86d3ac41
Lorenzo Giacosa <giacosalorenzo89@gmail.com>
2017-02-01 16:44:02 +0100

    Now is CKEDITOR that adds element into content array.

--- ksweb/ksweb/public/javascript/ckeditor/plugins/objplaceholder/plugin.js
+++ ksweb/ksweb/public/javascript/ckeditor/plugins/objplaceholder/plugin.js
@@ -19,12 +19,51 @@ CKEDITOR.plugins.add( 'objplaceholder', {
 
 
             downcast: function(el) {
-                var classes = el.attributes.class.split(" ");
-                var downcast_value = '';
-                for (var i=0; i<classes.length; i++)
-                    if (classes[i].startsWith('ks_id-'))
-                        downcast_value = classes[i].slice('ks_id-'.length);
 
+                var ractive_instance;
+
+                if (typeof ractive_output !== 'undefined') {
+                    ractive_instance = ractive_output;
+                } else if (typeof ractive_document !== 'undefined'){
+                    ractive_instance = ractive_document;
+                } else {
+                    console.log("ERROR: unexpected ractive template!");
+                    return;
+                }
+
+                // clear array
+                ractive_instance.set('create.content', []);
+
+                var type_ = '';
+                var id_ = '';
+                var downcast_value = '';
+
+
+                if (el.hasClass( 'qa-widget' )){
+                    type_ = 'qa_response'
+                } else if (el.hasClass( 'output-widget' )){
+                    type_ = 'output';
+                } else {
+                    console.log("ERROR: unexpected widget class!");
+                    return;
+                }
+
+                var classes = el.attributes.class.split(" ");
+
+                for (var i=0; i<classes.length; i++)
+                    if (classes[i].startsWith('ks_id-')){
+                        downcast_value = classes[i].slice('ks_id-'.length);
+                        id_ =  downcast_value.split('_')[1];
+                        break;
+                }
+
+                ractive_instance.get('create.content').push({
+                    'type': type_,
+                    'content': id_,
+                    'title': el.children[0].value
+                });
+
+                //TODO: escape chars...
                 return new CKEDITOR.htmlParser.text( '${' + downcast_value + '}' );
             },
 
