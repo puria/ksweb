d4e510226b24362a969d7637b9e2f41bd7362ac6
Puria Nafisi Azizi <nafisi@gmail.com>
2017-02-08 09:29:59 +0100

    Configuring the editor bar

--- ksweb/ksweb/public/javascript/ckeditor/config.js
+++ ksweb/ksweb/public/javascript/ckeditor/config.js
@@ -4,9 +4,28 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	 config.uiColor = '#f1f1f1';
-     config.extraPlugins = 'widget,lineutils,clipboard,widgetselection';
-     config.allowedContent = true;
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    config.uiColor = '#f1f1f1';
+    config.extraPlugins = 'widget,lineutils,clipboard,widgetselection';
+    config.allowedContent = true;
+
+    config.toolbarGroups = [
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+        { name: 'forms', groups: [ 'forms' ] },
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+        { name: 'links', groups: [ 'links' ] },
+        { name: 'insert', groups: [ 'insert' ] },
+        { name: 'document', groups: [ 'mode', 'doctools', 'document' ] },
+        '/',
+        { name: 'styles', groups: [ 'styles' ] },
+        { name: 'colors', groups: [ 'colors' ] },
+        { name: 'tools', groups: [ 'tools' ] },
+        { name: 'others', groups: [ 'others' ] },
+        { name: 'about', groups: [ 'about' ] }
+    ];
+
+    config.removeButtons = 'Source,Save,Templates,Scayt,Form,HiddenField,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,About,Flash,Smiley,SpecialChar,PageBreak,Iframe,Anchor,Unlink,Link,Styles,ShowBlocks,Language,BidiRtl,BidiLtr,CreateDiv,Blockquote,CopyFormatting,RemoveFormat,Superscript,Subscript,Strike,Replace,Find,SelectAll,NewPage';
 };
