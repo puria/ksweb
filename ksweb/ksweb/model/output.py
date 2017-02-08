1db69e61f3b3d2a470e60339de4d92ec4c390e54
Puria Nafisi Azizi <nafisi@gmail.com>
2017-02-08 11:47:57 +0100

    Better styling the editor components; Adding the preview of the output and documents

--- ksweb/ksweb/model/output.py
+++ ksweb/ksweb/model/output.py
@@ -17,7 +17,7 @@ def _custom_title(obj):
 
 
 def _content_preview(obj):
-    return Markup("Little preview of: %s" % obj._id)
+    return " ".join(Markup(obj.html).striptags().split()[:5])
 
 
 class Output(MappedClass):
